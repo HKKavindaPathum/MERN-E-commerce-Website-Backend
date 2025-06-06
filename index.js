adcf6afb11508e51f01a5dcfea2598fd5f9fad08
HKKavindaPathum <kavindapathum98@gmail.com>
2025-06-06 11:54:31 +0530

    env implementation

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import userRouter from './routes/userRouter.js';
 import jwt from "jsonwebtoken";
 import orderRouter from './routes/orderRoute.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -38,7 +40,7 @@ app.use(
 )
 
 //databse connnect
-mongoose.connect("mongodb+srv://kavinda:12345@cluster0.rz4ayzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGODB_URL)
 .then(
     ()=>{
         console.log("Connected to the database")
