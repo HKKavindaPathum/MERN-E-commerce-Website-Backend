4f7931b15cb7c24b96cc8d4c70aea473fc52e157
HKKavindaPathum <kavindapathum98@gmail.com>
2025-06-17 14:35:15 +0530

    error handel

--- models/product.js
+++ models/product.js
@@ -28,7 +28,7 @@ const productSchema = mongoose.Schema({
         type : Number,
         required : true
     },
-    stoke : {
+    stock : {
         type : Number,
         required : true
     },
